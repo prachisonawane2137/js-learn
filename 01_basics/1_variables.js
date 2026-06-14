2ee98eee7c1fb74622ceacca2608290308385347
prachisonawane2137 <125236970+prachisonawane2137@users.noreply.github.com>
2026-06-14 10:51:02 +0000

    variables

---
+++ 01_basics/1_variables.js
@@ -0,0 +1,10 @@
+const userId=104
+let userName="prachis"
+var city
+userEmail="prachi@gmail.com"
+
+console.table([userId, userName,city,userEmail])
+
+
+/* prefer not to use  var
+ because issue in block scope and function scope*/
---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const userId=10112
+let userName="Prachi"
+var city="jaypur"
+userEmail="prachi@gmail.com"
+let contact
+
+// console.log(userId);
+
+console.table([userId, userName, city,userEmail, contact])
+
+
+/* prefer not to use  var
+ because issue in block scope and function scope*/
