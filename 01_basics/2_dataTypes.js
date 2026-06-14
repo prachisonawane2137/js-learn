21a7b37c4a2e59498ea26c9ea0a2209ea7053625
prachisonawane2137 <125236970+prachisonawane2137@users.noreply.github.com>
2026-06-14 10:53:23 +0000

    DataTypes

---
+++ 01_basics/2_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treats all js code as newer version
+
+// alert(3+3) // we are using in nodejs, not browser
+
+let name="prachi"
+let age= 25
+let isLoggedIn =false
+
+//number - rang of 2 to 53
+// bigInt
+//string- ""
+//boolean - true/ false
+// null - standalone value/ empty
+// undefined -
+// symbol - unique
+//object
+
+// find the type of datatype
+
+console.log(typeof age)
+console.log (typeof( name)) // using method
+
+//Type of null is object
+// type of undefined is undefined
