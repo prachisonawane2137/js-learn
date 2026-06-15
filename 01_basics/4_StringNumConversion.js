f8d57847a13d4a6d3972b5e409e430c108668258
prachisonawane2137 <125236970+prachisonawane2137@users.noreply.github.com>
2026-06-15 11:44:13 +0000

    String To number conversion is confusion

---
+++ 01_basics/4_StringNumConversion.js
@@ -0,0 +1,9 @@
+let str1="hello"
+let str2="prachi"
+let str3=str1+str2
+console.log(str3)
+console.log("1"+2)
+console.log(1+"2")
+console.log(1+2+"2") // number first all numbers treat as a number
+console.log("1"+2+2)// string first all numbers treat as a string
+console.log(true)// true
