eb30ffb5f8bf5a702f9d331e79cecff075262b06
prachisonawane2137 <125236970+prachisonawane2137@users.noreply.github.com>
2026-06-16 11:28:34 +0000

    Comparison between two datatypes

---
+++ 01_basics/5_dataTypeComparison.js
@@ -0,0 +1,15 @@
+// let age= 23
+// let age1=54
+
+// console.log(age >age1)
+// console.log(age < age1)
+// console.log(age >= age1)
+// console.log(age <= age1)
+// console.log(age == age1)
+// console.log(age != age1)
+// console.log(age === age1)
+let string ="123"
+let num =123
+
+console.log(string === num)// comparison between datatypes
+
