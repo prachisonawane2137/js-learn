7d16c9d9dcd2640192cd6de76866c1168a72dd35
prachisonawane2137 <125236970+prachisonawane2137@users.noreply.github.com>
2026-06-18 11:38:47 +0000

    String Methods

---
+++ 01_basics/8_String.js
@@ -0,0 +1,50 @@
+// let name="prachi"
+// let amount =45
+
+
+
+// console.log(`The name of customer is ${name} and amount is ${amount}`)
+
+
+// string methods 
+
+let str="Hello"
+let str1= "   Prachi    "
+//1. concat
+
+
+// console.log(str+str1)
+
+//2. Length()
+
+// console.log(str.length)
+
+
+//3. indexOf
+// console.log(str.indexOf("e"))
+
+//4. charAt()
+
+// console.log(str1.charAt(5))
+
+//5.toLowerCase
+
+// console.log(str1.toLowerCase())
+
+//6.toUpperCase()
+
+// console.log(str1.toUpperCase())
+
+
+// 7. repalce()
+
+// console.log(str.replace("e", "a"))
+
+//8.slice()
+
+// console.log(str.slice(2,3))
+
+
+// 9.trim()
+console.log(str1)
+console.log(str1.trim())
