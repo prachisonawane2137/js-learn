b955fb9853b056df3fa3025c7b5a0c6f81903574
prachisonawane2137 <125236970+prachisonawane2137@users.noreply.github.com>
2026-06-19 10:31:21 +0000

    Numbers

---
+++ 01_basics/9_Numbers.js
@@ -0,0 +1,7 @@
+const account= new Number(200)
+console.log(account)
+console.log(account.toString())
+console.log(account.toString().length)
+console.log(account.toFixed(2))
+const num=100000
+console.log(num.toLocaleString(`en-IN`))
