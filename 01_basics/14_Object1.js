64bbfd4a4a7060213d82f6e5dae5e28430ae4c2f
prachisonawane2137 <125236970+prachisonawane2137@users.noreply.github.com>
2026-06-28 12:04:30 +0000

    Object Deconstruct

--- 01_basics/14_Object1.js
+++ 01_basics/14_Object1.js
@@ -1,71 +1,72 @@
-// const tender= new Object()
+// // const tender= new Object()
 
 
-const tender= {}
+// const tender= {}
 
-tender.id="prachi",
-tender.email="prachi@gmail.com",
-tender.city="Nashik"
-tender.isLoggedIn=false
-
-
-const regularUser ={
+// tender.id="prachi",
+// tender.email="prachi@gmail.com",
+// tender.city="Nashik"
+// tender.isLoggedIn=false
 
 
-    age:26,
-    fullName:{
-        userName:{
-            firstname:"prachi",
-            lastname:"sonawane"
-        }
+// const regularUser ={
+
+
+//     age:26,
+//     fullName:{
+//         userName:{
+//             firstname:"prachi",
+//             lastname:"sonawane"
+//         }
         
 
-        }
-    }
+//         }
+//     }
 
 
 
-// console.log(regularUser.fullName.userName.firstname.lastname)
+// // console.log(regularUser.fullName.userName.firstname.lastname)
 
-const obj1={1:"a", 2:"b"}
-const obj2={3:"c", 4:"d"}
-const obj3={5:"e", 6:"f"}
+// const obj1={1:"a", 2:"b"}
+// const obj2={3:"c", 4:"d"}
+// const obj3={5:"e", 6:"f"}
 
-// const obj={obj1, obj2, obj3}
+// // const obj={obj1, obj2, obj3}
 
-// const obj=Object.assign({}, obj1, obj2, obj3)  // assign() method like a concat ( ) method
+// // const obj=Object.assign({}, obj1, obj2, obj3)  // assign() method like a concat ( ) method
 
-const obj={...obj1, ...obj2, ...obj3}
-// console.log(obj)
-
-
-const user1=[
-
-   {
-    id:1,
-    email:"prachi@gmail.com"
-   } ,
-    {
- id:2,
-    email:"prachi@gmail.com"
-    },
-     {
-     id:3,
-    email:"prachi@gmail.com"
-   } ,
-    {
- id:4,
-    email:"prachi@gmail.com"
-    }
-]
-// console.log(user1[0])
+// const obj={...obj1, ...obj2, ...obj3}
+// // console.log(obj)
 
 
-// console.log(Object.keys(tender))// display the key insertend into object
-// console.log(Object.values(tender))// dispaly the  value of key
-// console.log(Object.entries(tender))// diplay key value pair of object
+// const user1=[
 
-console.log(Object.hasOwnProperty('isLoggedIn'))// is logged in avilable or not
+//    {
+//     id:1,
+//     email:"prachi@gmail.com"
+//    } ,
+//     {
+//  id:2,
+//     email:"prachi@gmail.com"
+//     },
+//      {
+//      id:3,
+//     email:"prachi@gmail.com"
+//    } ,
+//     {
+//  id:4,
+//     email:"prachi@gmail.com"
+//     }
+// ]
+// // console.log(user1[0])
+
+
+// // console.log(Object.keys(tender))// display the key insertend into object
+// // console.log(Object.values(tender))// dispaly the  value of key
+// // console.log(Object.entries(tender))// diplay key value pair of object
+
+// console.log(Object.hasOwnProperty('isLoggedIn'))// is logged in avilable or not
+
 
 
 
---
+++ 01_basics/15_Object_Deconstruct.js
@@ -0,0 +1,15 @@
+const course={
+
+    coursename:"Js Learn",
+    price:999,
+    courceInstructor:"prachi"
+
+}
+
+const {courceInstructor: Instructor}=course
+
+
+console.log(Instructor)
+
+
+
