e13bbc7538949f29ca4c303e0d654aadd3313d90
prachisonawane2137 <125236970+prachisonawane2137@users.noreply.github.com>
2026-06-30 12:26:04 +0000

    Function with Array

---
+++ 01_basics/17_FunctionArray.js
@@ -0,0 +1,44 @@
+// function Arrays(num1){
+//     return num1
+// }
+// console.log(Arrays(12,34,45,56))// only print first value  o/p: 12
+
+
+
+function Arrays (...num1){
+    return num1
+}
+
+// console.log(Arrays(10,20,30,40))// print all value
+
+
+
+// function Array(val1, val2, ...num1){
+//     return num1
+// }
+// console.log(Array(12,3,4,5,67,9)) // o/p: [4,5,67,9]  val1 and val2 not printed
+ 
+
+
+// //object
+// const user={
+//     username:"prachi",
+//     price:999
+// }
+
+//how to write object in function
+
+// function handleObject (Anyone){
+//     console.log(`The username is ${Anyone.username} and price is ${Anyone.price}`);
+// }
+// handleObject(user)
+
+//  Array in  fuction
+
+// const newArray=[23,45,56,67,56]
+
+// function arrayfunction(getArray){
+//     return getArray[2]
+// }
+// console.log(arrayfunction(newArray))
+
