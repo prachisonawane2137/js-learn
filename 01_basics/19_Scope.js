666ca5ae6655c191e640bbf2080c2f9edcc723a3
prachisonawane2137 <125236970+prachisonawane2137@users.noreply.github.com>
2026-07-02 17:24:45 +0000

    scope of function or If

---
+++ 01_basics/19_Scope.js
@@ -0,0 +1,34 @@
+
+// function one(){
+//     username="prachi"
+//     function two(){
+//         age=23
+//         console.log(username)// we can access the property of parent in a child
+
+//     }
+//     console.log(age)// we can not access property of one or parent function
+//     two()
+// }
+// one()
+
+
+
+// if(true){
+//     const username="prachi"
+//     if(username==="prachi"){
+//         const website="utube"
+//         console.log(username+ website)
+//     }
+//     console.log(website)
+// }
+// console.log(username)
+
+one(5)
+function one(num){
+    return num+1
+}
+
+two (5)// can not access before initialization
+const two= function(num1){
+    return num1+2
+}
