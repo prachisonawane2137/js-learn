a2b6dab02a1f4a94ad67726e6d7f5c1dc3e110e7
prachisonawane2137 <125236970+prachisonawane2137@users.noreply.github.com>
2026-07-07 12:01:42 +0000

    Swtich cases Staement

---
+++ 01_basics/24_Switch.js
@@ -0,0 +1,52 @@
+
+// check with number
+// const month=6
+// switch(month){
+//     case 1:
+//         console.log("Jan");
+//         break;
+
+//     case 2: 
+//     console.log("feb");
+//     break;
+
+//     case 3:
+//         console.log("March");
+//     break;
+
+//     case 4:
+//         console.log("April");
+//         break;
+    
+//     default :
+//     console.log(" not exist")
+//     break;
+
+// }
+
+
+// check with string
+
+const month="March"
+switch(month){
+    case "Jan":
+        console.log("Jan");
+        break;
+
+    case "Feb": 
+    console.log("feb");
+    break;
+
+    case "March":
+        console.log("March");
+    break;
+
+    case "April":
+        console.log("April");
+        break;
+    
+    default :
+    console.log(" not exist")
+    break;
+
+}
