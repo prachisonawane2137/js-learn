a9ab13bd3fb0b54b7aac3311eba66c46a7f4730c
prachisonawane2137 <125236970+prachisonawane2137@users.noreply.github.com>
2026-07-07 12:01:13 +0000

    Conditional Statement ( If, IF Else, Nested If Else, checked empty array, cheked empty object , truthy and falusey values,  Nullish coalescing operator)

---
+++ 01_basics/23_ConditionalStatement.js
@@ -0,0 +1,88 @@
+
+// IF statement
+
+
+// let price=100
+
+// if(price==100){
+//     console.log("executed")
+// }
+
+
+// IF Else Statement
+
+// let temp=40
+
+// if(temp<35){
+//     console.log("temp is grater than 35")
+// } else{
+//     console.log("temp is 40")
+// }
+
+
+// Nested IF Else loop
+
+
+// let amount = 10
+
+// if(amount <= 500){
+//   console.log("the amount is less than 1000")
+// }else if(amount <700){
+//  console.log("the amount is less than 700")
+// }else{
+//     console.log("the amount is 1020")
+// }
+
+
+// check multiple condition in if else
+
+
+    // const userLoggedIn= true
+    // const  debitcard= true
+    // const loggedInUserFromGoolge=false
+    // const loggedInUserFromEmail=true
+
+    // if(userLoggedIn && debitcard){
+    //     console.log("Allow to user buy courses")
+    // }
+
+    // if(loggedInUserFromEmail ||  loggedInUserFromGoolge){
+    //     console.log("User logged in successfully..")
+    // }
+
+
+    // Nullish coalescing Operator (?? )  this operator for null and undefind
+
+  let val1;
+//   val1= 5 ?? 10   o/p:  5
+// val1= null ?? 10  o/p: 10
+
+// val1 =undefined ?? 10   o/p: 10
+
+//  val1= null ?? 10 ?? 20   o/p: 10
+  console.log(val1)
+
+
+    // falsy value
+    // false, 0, -0, "", bigInt 0n, undefine, NaN, null
+
+    // truthy values 
+    // 'false', " ", [], {}, function(){},"0"
+
+
+    // how to written array is empty
+
+
+    // const userEmail=[]
+
+    // if(userEmail.length==0){
+    //     console.log("The Array is empty")
+    // }
+
+    // how to check object is empty
+
+    // const emptyObj={}
+
+    // if(Object.keys(emptyObj).length===0){
+    //     console.log("Object is empty")
+    // }
