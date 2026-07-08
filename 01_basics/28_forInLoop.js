a50b534fc8e8b70f4776c9b0c6c6f69ea08ee170
prachisonawane2137 <125236970+prachisonawane2137@users.noreply.github.com>
2026-07-08 13:13:21 +0000

    for in loop for object and array

---
+++ 01_basics/28_forInLoop.js
@@ -0,0 +1,36 @@
+
+
+
+// for in loop for object iteration
+
+
+// const myObject={
+
+//     username:'prachi',
+//     surname:'sonawane'
+// }
+// for(const key in myObject){
+//     console.log(`${key} : ${myObject[key]}`)
+// }
+
+
+// for in loop for array
+
+
+// const lang=["java", "php","js","python"]
+// for(const key in lang){
+//     console.log(lang[key])
+// }// it is applied
+
+
+
+// const map =new Map()
+// map.set('IN',"India")
+// map.set('UK', "United State")
+// map.set('IN',"India")
+
+
+// for(const key in map){
+//  console.log(key );
+ 
+//}// for in loop not working in map 
