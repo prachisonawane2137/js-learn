406040485cc1c5a3a04acf0d7a9ad4e1b4e9fb40
prachisonawane2137 <125236970+prachisonawane2137@users.noreply.github.com>
2026-07-08 09:49:31 +0000

    For Loop, for loop include if statement,  break and continue keyword

---
+++ 01_basics/25_ForLoops.js
@@ -0,0 +1,59 @@
+
+
+// for(let i=0;i<=10;i++){
+//     console.log(i)
+// }
+
+// for loop includes if statement
+
+// for (let i=0; i<=10;i++){
+//     if(i==5){
+//         console.log(`${i} is printed`)
+//     }
+//     console.log(i)
+// }
+
+// Two for loops
+
+
+// for(let i=0;i<=10;i++){
+
+//     for(let j=0; j<=10;j++){
+//         console.log(i*j )
+//     }
+   
+// }
+
+
+
+// For loop with Array
+
+
+// const myArray=["prachi", "vijay","Sonu","Teju"]
+
+// for(let i=0;i<myArray.length;i++){
+//     console.log(myArray[i])
+// }
+
+// break keyword
+
+
+// for(let i=0; i<=5;i++){
+//     if(i==2){
+//         console.log(`${i} is equal`)
+//         break
+//     }
+//     console.log(i)
+// }  // o/p: 0, 1, 2 is equal 
+
+
+
+// Continue keyword
+
+for (let i=0;i<=5;i++){
+    if(i==2){
+        console.log(`${i}, is equal`)
+        continue
+    }
+    console.log(i)// o/p: 0,1,2 is equal, 3, 4, 5
+}
