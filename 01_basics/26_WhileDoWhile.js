f93b3313f55415ecc19baea381eb74ea54cb5b83
prachisonawane2137 <125236970+prachisonawane2137@users.noreply.github.com>
2026-07-08 10:15:07 +0000

    while and do while loop

---
+++ 01_basics/26_WhileDoWhile.js
@@ -0,0 +1,31 @@
+
+
+// while loop
+
+// let i=0
+
+// while(i<=10){
+//     console.log(i)
+//      i++
+// }
+
+//While loop with array
+
+
+// let index=["prachi", "vijay", "sonu"]
+// let i=0
+// while(i<index.length){
+//     console.log(index[i])
+//     i++
+// }
+
+
+
+// Do while loop
+
+let i=1
+
+do{
+    console.log(i);
+    i++
+}while(i<=5)
